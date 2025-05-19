0d9f5b7f78046cfe0680be988f06cea6edd800f2
karosi12 <adekayor@gmail.com>
2025-05-19 19:13:17 +0100

    revert kubeconfig output

--- 0-eks/modules/eks/outputs.tf
+++ 0-eks/modules/eks/outputs.tf
@@ -10,35 +10,35 @@ output "oidc_url" {
   value = aws_iam_openid_connect_provider.eks.url
 }
 
-output "kubeconfig" {
-  value = "aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.opsfleet.name}"
-}
-
 # output "kubeconfig" {
-#   value = <<EOT
-#   apiVersion: v1
-#   kind: Config
-#   clusters:
-#   - name: ${aws_eks_cluster.opsfleet.name}
-#     cluster:
-#       server: ${aws_eks_cluster.opsfleet.endpoint}
-#       certificate-authority-data: ${aws_eks_cluster.opsfleet.certificate_authority[0].data}
-#   contexts:
-#   - name: ${aws_eks_cluster.opsfleet.name}
-#     context:
-#       cluster: ${aws_eks_cluster.opsfleet.name}
-#       user: aws
-#   current-context: ${aws_eks_cluster.opsfleet.name}
-#   users:
-#   - name: aws
-#     user:
-#       exec:
-#         apiVersion: client.authentication.k8s.io/v1beta1
-#         command: aws
-#         args:
-#         - eks
-#         - get-token
-#         - --region
-#         - ${var.region}
-#   EOT
+#   value = "aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.opsfleet.name}"
 # }
+
+output "kubeconfig" {
+  value = <<EOT
+  apiVersion: v1
+  kind: Config
+  clusters:
+  - name: ${aws_eks_cluster.opsfleet.name}
+    cluster:
+      server: ${aws_eks_cluster.opsfleet.endpoint}
+      certificate-authority-data: ${aws_eks_cluster.opsfleet.certificate_authority[0].data}
+  contexts:
+  - name: ${aws_eks_cluster.opsfleet.name}
+    context:
+      cluster: ${aws_eks_cluster.opsfleet.name}
+      user: aws
+  current-context: ${aws_eks_cluster.opsfleet.name}
+  users:
+  - name: aws
+    user:
+      exec:
+        apiVersion: client.authentication.k8s.io/v1beta1
+        command: aws
+        args:
+        - eks
+        - get-token
+        - --region
+        - ${var.region}
+  EOT
+}
